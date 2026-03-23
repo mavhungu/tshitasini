f87d5a18846efb35a5fdad22dae70a2cc5bee1a3
ronewa <ronewamavhungu@gmail.com>
2026-03-23 18:39:44 +0200

    fixing pool error

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,14 +5,18 @@
 // ============================================================
 
 import "dotenv/config"
-import { Pool } from "pg"
+// import { Pool } from "pg"
 import { PrismaPg } from "@prisma/adapter-pg"
 import { PrismaClient } from "../lib/generated/prisma/client";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
-const pool = new Pool({ connectionString });
-const adapter = new PrismaPg(pool);
+// const pool = new Pool({ connectionString });
+if(!connectionString) {
+  throw new Error('DATABASE_URL is not set')
+}
+
+const adapter = new PrismaPg({connectionString});
 const prisma = new PrismaClient({ adapter });
 
 // ─────────────────────────────────────────────────────────────
